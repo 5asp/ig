7cf813e8367e85f658ab3f5b82574bc4886277a9
dev <xxuechin@gmail.com>
2023-03-30 01:08:34 +0800

    modify model

---
+++ transports/grpc.go
@@ -0,0 +1,44 @@
+package transport
+
+import (
+	"context"
+
+	"github.com/aszeta/ig/endpoints"
+	"github.com/aszeta/ig/pb"
+	"github.com/go-kit/kit/log"
+	gt "github.com/go-kit/kit/transport/grpc"
+)
+
+type gRPCServer struct {
+	pb.UnimplementedMathServiceServer
+	add gt.Handler
+}
+
+// NewGRPCServer initializes a new gRPC server
+func NewGRPCServer(endpoints endpoints.Endpoints, logger log.Logger) pb.MathServiceServer {
+	return &gRPCServer{
+		add: gt.NewServer(
+			endpoints.Add,
+			decodeMathRequest,
+			encodeMathResponse,
+		),
+	}
+}
+
+func (s *gRPCServer) Add(ctx context.Context, req *pb.MathRequest) (*pb.MathResponse, error) {
+	_, resp, err := s.add.ServeGRPC(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return resp.(*pb.MathResponse), nil
+}
+
+func decodeMathRequest(_ context.Context, request interface{}) (interface{}, error) {
+	req := request.(*pb.MathRequest)
+	return endpoints.MathReq{NumA: req.NumA, NumB: req.NumB}, nil
+}
+
+func encodeMathResponse(_ context.Context, response interface{}) (interface{}, error) {
+	resp := response.(endpoints.MathResp)
+	return &pb.MathResponse{Result: resp.Result}, nil
+}
